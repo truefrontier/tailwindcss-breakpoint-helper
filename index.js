ede4e8e85c83bdf0b3b44d25052ada2a654fcbdd
Kevin Kirchner <kirchner.kevin@gmail.com>
2020-04-23 11:37:04 -0500

    adding dependecies; restructuring export so it works

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-module.exports = function({ addComponents, theme }) {
-  const screens = theme('screens');
-  const userOptions = theme('breakpointHelper.options', {
-    selector: '.breakpoint-helper::after',
-  });
-  let component = {};
-  component[`${userOptions.selector}`] = {
-    position: 'fixed',
-    zIndex: '2147483647',
-    left: '0',
-    bottom: '1rem',
-    padding: '0.33rem 0.5rem',
-    borderRadius: '0 0.33rem 0.33rem 0',
-    fontSize: '1rem',
-    lineHeight: '1',
-    fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`,
-    backgroundColor: '#000',
-    color: '#fff',
-    content: `'[none]: 0px'`,
-  };
+module.exports = {
+  plugins: [
+    function({ addComponents, theme }) {
+      const screens = theme('screens');
+      const userOptions = theme('breakpointHelper.options', {
+        selector: '.breakpoint-helper::after',
+      });
+      let component = {};
+      component[`${userOptions.selector}`] = {
+        position: 'fixed',
+        zIndex: '2147483647',
+        left: '0',
+        bottom: '1rem',
+        padding: '0.33rem 0.5rem',
+        borderRadius: '0 0.33rem 0.33rem 0',
+        fontSize: '1rem',
+        lineHeight: '1',
+        fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`,
+        backgroundColor: '#000',
+        color: '#fff',
+        content: `'[none]: 0px'`,
+      };
 
-  Object.entries(screens).forEach(([screen, value]) => {
-    let style = {};
-    style[`${userOptions.selector}`] = {
-      content: `'${screen}: ${value}'`,
-    };
-    component[`@screen ${screen}`] = style;
-  });
+      Object.entries(screens).forEach(([screen, value]) => {
+        let style = {};
+        style[`${userOptions.selector}`] = {
+          content: `'${screen}: ${value}'`,
+        };
+        component[`@screen ${screen}`] = style;
+      });
 
-  addComponents(component);
+      addComponents(component);
+    },
+  ],
 };
