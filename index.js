2ce68307072e17f79ea6b0cc448442de48882019
Kevin Kirchner <kirchner.kevin@gmail.com>
2020-04-23 14:25:25 -0500

    update styles; add options

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 module.exports = function({ addComponents, theme }) {
   const screens = theme('screens');
-  const userOptions = theme('breakpointHelper.options', {
-    selector: '.breakpoint-helper',
-  });
+
+  const selector = theme('breakpointHelper.selector', '.breakpoint-helper');
+  const pseudo = theme('breakpointHelper.pseudo', '::after');
+  const hideOnHover = theme('breakpointHelper.hideOnHover', true);
+  const userStyles = theme('breakpointHelper.style', {});
+
+  const defaultPosition = ['bottom', 'left'];
+  const position = theme('breakpointHelper.position', defaultPosition);
+  const positionY = position[0] || defaultPosition[0];
+  const positionX = position[1] || defaultPosition[1];
+
   let component = {};
-  component[`${userOptions.selector}::after`] = {
-    position: 'fixed',
-    zIndex: '2147483647',
-    left: '0',
-    bottom: '1rem',
-    padding: '0.33rem 0.5rem',
-    borderRadius: '0 0.33rem 0.33rem 0',
-    fontSize: '1rem',
-    lineHeight: '1',
-    fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`,
-    backgroundColor: '#000',
-    color: '#fff',
-    content: `'[none]: 0px'`,
-  };
-  component[`${userOptions.selector}:hover::after`] = {
-    display: 'none',
-  };
+
+  component[`${selector}${pseudo}`] = Object.assign(
+    {
+      position: 'fixed',
+      zIndex: '2147483647',
+      [positionY]: '0',
+      [positionX]: '0',
+      padding: '0.33rem 0.5rem',
+      borderRadius: '2px',
+      fontSize: '0.75rem',
+      lineHeight: '1rem',
+      fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`,
+      backgroundColor: '#000',
+      color: '#fff',
+      content: `'default (0px)'`,
+    },
+    userStyles,
+  );
+
+  if (hideOnHover) {
+    component[`${selector}:hover${pseudo}`] = {
+      display: 'none',
+    };
+  }
 
   Object.entries(screens).forEach(([screen, value]) => {
     let style = {};
-    style[`${userOptions.selector}::after`] = {
-      content: `'${screen}: ${value}'`,
+    style[`${selector}${pseudo}`] = {
+      content: `'${screen} (${value})'`,
     };
     component[`@screen ${screen}`] = style;
   });
